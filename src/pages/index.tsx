8335d499470c4c65da4205dc6593da07e72a3633
Maike Muzitano <maikemuzitano@gmail.com>
2023-09-28 15:08:36 -0300

    calendar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,6 +37,7 @@ export default function Home() {
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
             <div className="flex items-center space-x-2">
+              <CalendarDateRangePicker />
               <Button className="hover:scale-105">Download</Button>
             </div>
           </div>
